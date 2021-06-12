893f00f5b9a72cf9cf370f479ee4026abe1657a6
Lasse Leegaard <lasse@brandbil.dk>
2021-06-13 00:05:16 +0200

    Make ping() do actual work and change Validation() to use ping()

--- nerve.py
+++ nerve.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+import subprocess
 
-def ping():
-    return 'success'
+
+def ping(host='127.0.0.1'):
+    ping_switch = '-c'
+    ping_count = '1'
+    ping_destination = host
+    ping_command = 'ping' + ' ' + ping_switch + ' ' + ping_count + ' ' + ping_destination
+    ping_output = subprocess.run(ping_command, shell=True)
+    return ping_output.returncode
+
+
+class Validation:
+    def __init__(self, validation_type, destination='brandbil.dk'):
+        if validation_type == 'ping': self.returncode = ping(destination)
+        self.destination = destination
--- test_nerve.py
+++ test_nerve.py
@@ -1,5 +1,27 @@
 import nerve
 
 
-def test_nerve():
-    assert nerve.ping() == 'success'
+class TestValidation:
+    def test_ping_nothing(self):
+        assert nerve.ping() == 0
+
+    def test_ping_brandbil_dot_dk(self):
+        assert nerve.ping('brandbil.dk') == 0
+
+    def test_ping_dr_dot_dk(self):
+        assert nerve.ping('dr.dk') == 0
+
+    def test_ping_validation_nothing(self):
+        validation = nerve.Validation('ping', '')
+        assert validation.destination == ''
+        assert validation.returncode == 1
+
+    def test_ping_validation_brandbil_dot_dk(self):
+        validation = nerve.Validation('ping','brandbil.dk')
+        assert validation.destination == 'brandbil.dk'
+        assert validation.returncode == 0
+
+    def test_ping_validation_should_fail_for_doesnotexist_dot_brandbil_dot_dk(self):
+        validation = nerve.Validation('ping', 'doesnotexist.brandbil.dk')
+        assert validation.destination == 'doesnotexist.brandbil.dk'
+        assert validation.returncode == 2
