320697c295c42cc7de22e2aa0d080989c06a862b
Lasse Leegaard <lasse@brandbil.dk>
2021-10-06 00:35:43 +0200

    Rename project to valis

--- test_valis.py
+++ test_valis.py
@@ -1,27 +1,27 @@
-import nerve
+import valis
 
 
 class TestValidation:
     def test_ping_nothing(self):
-        assert nerve.ping() == 0
+        assert valis.ping() == 0
 
     def test_ping_brandbil_dot_dk(self):
-        assert nerve.ping('brandbil.dk') == 0
+        assert valis.ping('brandbil.dk') == 0
 
     def test_ping_dr_dot_dk(self):
-        assert nerve.ping('dr.dk') == 0
+        assert valis.ping('dr.dk') == 0
 
     def test_ping_validation_nothing(self):
-        validation = nerve.Validation('ping', '')
+        validation = valis.Validation('ping', '')
         assert validation.destination == ''
         assert validation.returncode == 1
 
     def test_ping_validation_brandbil_dot_dk(self):
-        validation = nerve.Validation('ping','brandbil.dk')
+        validation = valis.Validation('ping', 'brandbil.dk')
         assert validation.destination == 'brandbil.dk'
         assert validation.returncode == 0
 
     def test_ping_validation_should_fail_for_doesnotexist_dot_brandbil_dot_dk(self):
-        validation = nerve.Validation('ping', 'doesnotexist.brandbil.dk')
+        validation = valis.Validation('ping', 'doesnotexist.brandbil.dk')
         assert validation.destination == 'doesnotexist.brandbil.dk'
         assert validation.returncode == 2
