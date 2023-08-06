1adbdc0b4264f27478a65c28dcb9ad954f27f5af
Lasse Leegaard <lasse@brandbil.dk>
2023-08-06 13:48:55 +0200

    Black formatting

--- test_valis.py
+++ test_valis.py
@@ -6,30 +6,30 @@ def test_ping_nothing():
 
 
 def test_ping_brandbil_dot_dk():
-    assert valis.ping('brandbil.dk') == 0
+    assert valis.ping("brandbil.dk") == 0
 
 
 def test_ping_dr_dot_dk():
-    assert valis.ping('dr.dk') == 0
+    assert valis.ping("dr.dk") == 0
 
 
 def test_ping_validation_nothing():
-    validation = valis.Validation('ping', '')
-    assert validation.destination == ''
+    validation = valis.Validation("ping", "")
+    assert validation.destination == ""
     assert validation.returncode == 64 or 1
     # Ping return codes for no destination are different for different OSes:
     # 64 on OSX, 1 on linux, 1 on FreeBSD
 
 
 def test_ping_validation_brandbil_dot_dk():
-    validation = valis.Validation('ping', 'brandbil.dk')
-    assert validation.destination == 'brandbil.dk'
+    validation = valis.Validation("ping", "brandbil.dk")
+    assert validation.destination == "brandbil.dk"
     assert validation.returncode == 0
 
 
 def test_ping_validation_should_fail_for_doesnotexist_dot_brandbil_dot_dk():
-    validation = valis.Validation('ping', 'doesnotexist.brandbil.dk')
-    assert validation.destination == 'doesnotexist.brandbil.dk'
+    validation = valis.Validation("ping", "doesnotexist.brandbil.dk")
+    assert validation.destination == "doesnotexist.brandbil.dk"
     assert validation.returncode == 68 or 2 or 1
     # Ping return codes for non-existing domain name are different for different OSes:
     # 68 on OSX, 2 on linux, 1 on FreeBSD
--- valis.py
+++ valis.py
@@ -2,20 +2,19 @@
 from subprocess import run
 
 
-def ping(host='127.0.0.1'):
-    ping_switch = '-c'
-    ping_count = '1'
+def ping(host="127.0.0.1"):
+    ping_switch = "-c"
+    ping_count = "1"
     ping_destination = host
-    ping_command = ('ping' + ' ' +
-                    ping_switch + ' ' +
-                    ping_count + ' ' +
-                    ping_destination)
+    ping_command = (
+        "ping" + " " + ping_switch + " " + ping_count + " " + ping_destination
+    )
     ping_output = run(ping_command, shell=True)
     return ping_output.returncode
 
 
 class Validation:
-    def __init__(self, validation_type, destination='brandbil.dk'):
-        if validation_type == 'ping':
+    def __init__(self, validation_type, destination="brandbil.dk"):
+        if validation_type == "ping":
             self.returncode = ping(destination)
         self.destination = destination
