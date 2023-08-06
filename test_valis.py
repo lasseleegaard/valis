ac4c870c5530f40cf96a7b162854b3df0999b945
Lasse Leegaard <lasse@brandbil.dk>
2023-08-06 17:36:24 +0200

    Skip tests that does not work in Github anyway

--- test_valis.py
+++ test_valis.py
@@ -1,18 +1,27 @@
 import valis
+import os
+import pytest
 
 
+IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
+
+
+@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work in Github Actions.")
 def test_ping_nothing():
     assert valis.ping() == 0
 
 
+@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work in Github Actions.")
 def test_ping_brandbil_dot_dk():
     assert valis.ping("brandbil.dk") == 0
 
 
+@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work in Github Actions.")
 def test_ping_dr_dot_dk():
     assert valis.ping("dr.dk") == 0
 
 
+@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work in Github Actions.")
 def test_ping_validation_nothing():
     validation = valis.Validation("ping", "")
     assert validation.destination == ""
@@ -21,12 +30,14 @@ def test_ping_validation_nothing():
     # 64 on OSX, 1 on linux, 1 on FreeBSD
 
 
+@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work in Github Actions.")
 def test_ping_validation_brandbil_dot_dk():
     validation = valis.Validation("ping", "brandbil.dk")
     assert validation.destination == "brandbil.dk"
     assert validation.returncode == 0
 
 
+@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work in Github Actions.")
 def test_ping_validation_should_fail_for_doesnotexist_dot_brandbil_dot_dk():
     validation = valis.Validation("ping", "doesnotexist.brandbil.dk")
     assert validation.destination == "doesnotexist.brandbil.dk"
