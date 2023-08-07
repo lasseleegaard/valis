60602a091e01daafa71738ff80e64c669011f01b
Lasse Leegaard <lasse@brandbil.dk>
2023-08-07 02:50:33 +0200

    Add TCP feature

--- test_valis.py
+++ test_valis.py
@@ -44,3 +44,21 @@ def test_ping_validation_should_fail_for_doesnotexist_dot_brandbil_dot_dk():
     assert validation.returncode == 68 or 2 or 1
     # Ping return codes for non-existing domain name are different for different OSes:
     # 68 on OSX, 2 on linux, 1 on FreeBSD
+
+
+def test_tcp_validation_nothing():
+    validation = valis.Validation("tcp", "", 22)
+    assert validation.destination == ""
+    assert validation.returncode == 1
+
+
+def test_tcp_validation_brandbil_dot_dk():
+    validation = valis.Validation("tcp", "brandbil.dk", 22)
+    assert validation.destination == "brandbil.dk"
+    assert validation.returncode == 0
+
+
+def test_tcp_validation_should_fail_for_doesnotexist_dot_brandbil_dot_dk():
+    validation = valis.Validation("tcp", "doesnotexist.brandbil.dk", 22)
+    assert validation.destination == "doesnotexist.brandbil.dk"
+    assert validation.returncode != 0
